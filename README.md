4521f2bf789fdd05f89901edce96350d933dc15a
mertcakmak2 <mertcakmak2@gmail.com>
2022-02-19 01:53:05 +0300

    sonarqube with docker

---
+++ README.md
@@ -0,0 +1,1 @@
+![image](https://user-images.githubusercontent.com/21373505/154771744-3d78961a-185e-4394-a7c6-cb8d22f31d5a.png)
---
+++ src/main/java/com/example/sonarqube/SonarqubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarqubeApplication.class, args);
+    }
+
+}
